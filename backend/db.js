5a90fa23096c055e187cdd96ebf86d65d0affbe3
Prayag Bhatia <prayag.bhatia2015@vit.ac.in>
2022-10-26 10:43:44 +0530

    ADD : nodejs db & express server startup

---
+++ backend/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose') ;
+
+const mongoURI = 'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.0'
+
+const connectToMongo = () => {
+    mongoose.connect(mongoURI, () => {console.log('Connected to Mongo successfully')} )
+}
+
+module.exports = connectToMongo;
