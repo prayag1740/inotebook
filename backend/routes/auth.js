d11a1ed063dc155792a1fae3b83a2611727f0592
Prayag Bhatia <prayag.bhatia2015@vit.ac.in>
2022-10-28 19:48:07 +0530

    ADD : password hashing bcryptjs

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const User = require('../models/User')
 const {userValidationRules, validate } = require('../validator.js')
+const bcrypt = require('bcryptjs');
 
 const router = express.Router();
 
-//CREATE USER (POST) /api/auth/ (Without login)
-router.post("/", userValidationRules(), validate,  (req, res) => {
+//CREATE USER (POST) /api/auth/createuser (Without login)
+router.post("/createuser", userValidationRules(), validate,  
+    async (req, res) => {
+    const salt = await bcrypt.genSalt(10) ;
+    const secPass = await bcrypt.hash(req.body.password, salt) ;
+
+    req.body.password = secPass
     const user = User(req.body);
 
     user.save().then(user => res.json(user)).catch(function(err) {
